fa67c09661096af56178ff493237491a5ca1b67c
Adrian Grimm <usmcamgrimm@gmail.com>
2022-02-23 11:51:03 -0500

    Fetch API data with axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './index.css'
 import Header from './components/Header'
 
 function App() {
+  const [characters, setCharacters] = useState([])
+  const [systems, setSystems] = useState([])
+  const [species, setSpecies] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people/')
+      .then(res => {
+        setCharacters(res.data.results)
+      })
+      .catch(err => console.log(err))
+    axios
+      .get('https://swapi.dev/api/planets/')
+      .then(res => {
+        setSystems(res.data.results)
+      })
+      .catch(err => console.log(err))
+    axios
+      .get('https://swapi.dev/api/species/')
+      .then(res => {
+        setSpecies(res.data.results)
+      })
+      .catch(err => console.log(err))
+  }, [])
+  console.log(characters)
+  console.log(systems)
+  console.log(species)
   return (
     <div className='container'>
       <Header />
